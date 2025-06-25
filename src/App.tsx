afc42faef65eeeaf8fb6510e056fc8f87c4878b5
techDeckDude <108017459+techDeckDude@users.noreply.github.com>
2025-06-24 23:19:21 -0400

    Develop (#10)
    * Feature/blog data model (#1)
    
    * added blogpost data model
    
    * updating blog post ui
    
    ---------
    
    Co-authored-by: techdeckdude <justinbullock025@gmail.com>
    
    * migrated to external blog post type table (#2)
    
    Co-authored-by: Justin Bullock <jbullock@scholastic.com>
    
    * attempting to resolve build error
    
    * following amplify external data source config tutorial (#4)
    
    Co-authored-by: Justin Bullock <jbullock@scholastic.com>
    
    * attempting to resolve build error (#5)
    
    Co-authored-by: Justin Bullock <jbullock@scholastic.com>
    
    * Feature/migrate to new blogpost table (#6)
    
    * attempting to resolve build error
    
    * attempting to get custom queries working
    
    ---------
    
    Co-authored-by: Justin Bullock <jbullock@scholastic.com>
    
    * Feature/migrate to new blogpost table (#7)
    
    * attempting to resolve build error
    
    * attempting to get custom queries working
    
    * configuring blopostType table into amplify data client
    
    ---------
    
    Co-authored-by: Justin Bullock <jbullock@scholastic.com>
    
    * Feature/migrate to new blogpost table (#8)
    
    * attempting to resolve build error
    
    * attempting to get custom queries working
    
    * configuring blopostType table into amplify data client
    
    * attempting to resolve model type ref issue
    
    ---------
    
    Co-authored-by: Justin Bullock <jbullock@scholastic.com>
    
    * Feature/blog data model (#9)
    
    * added blogpost data model
    
    * updating blog post ui
    
    * updated ui styling
    
    ---------
    
    Co-authored-by: techdeckdude <justinbullock025@gmail.com>
    
    * trying to resolve build error
    
    * fixing build amplify issue
    
    ---------
    
    Co-authored-by: techdeckdude <justinbullock025@gmail.com>
    Co-authored-by: Justin Bullock <jbullock@scholastic.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,46 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import "./App.css";
 
 const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [blogPosts, setBlogPosts] = useState<Array<Schema["BlogPost"]["type"]>>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
+    client.models.BlogPost.observeQuery().subscribe({
+      next: (data) => setBlogPosts([...data.items]),
     });
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
+  function createBlogPost() {
+    const content = window.prompt("Enter your blog post content:");
+    if (content && content.trim()) {
+      client.models.BlogPost.create({
+        content: content.trim()
+      });
+    }
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+    <main className="blog-container">
+      <h1>My Blog Posts</h1>
+      <button onClick={createBlogPost}>✍️ Create New Post</button>
+      
+      {blogPosts.length > 0 ? (
+        <ul className="blog-posts-list">
+          {blogPosts.map((post) => (
+            <li key={post.id}>
+              {post.content}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty-state">
+          No blog posts yet. Click "Create New Post" to get started!
+        </div>
+      )}
     </main>
   );
 }
